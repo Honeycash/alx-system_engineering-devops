a817815603983b7c7f322087663c94bd496297c4
Honeycash <endurancechery@gmail.com>
2024-04-25 10:22:27 -0700

    Update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-
-import csv
+"""
+script to fetch api information
+"""
 import requests
 import sys
 
+api = "https://jsonplaceholder.typicode.com"
+"""api url"""
+
 
 if __name__ == "__main__":
-    # Get the user ID from the command-line arguments provided to the script
-    user_id = sys.argv[1]
-
-    # Define the base URL for the JSON API
-    url = "https://jsonplaceholder.typicode.com/"
-
-    # Fetch user information from the API and
-    #   convert the response to a JSON object
-    user = requests.get(url + "users/{}".format(user_id)).json()
-
-    # Extract the username from the user data
-    username = user.get("username")
-
-    # Fetch the to-do list items associated with the
-    #   given user ID and convert the response to a JSON object
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    # Use list comprehension to iterate over the to-do list items
-    # Write each item's details (user ID, username, completion status,
-    #   and title) as a row in the CSV file
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
-
+    all_list = []
+    if len(sys.argv) > 1:
+        id = int(sys.argv[1])
+        req = requests.get("{}/todos/".format(api))
+        user = requests.get("{}/users/{}".format(api, id)).json()
+        todos = list(filter(lambda x: x.get('userId') == id, req.json()))
+        file_name = "{}.csv".format(id)
+        with open(file_name, "w") as file:
+            for i in todos:
+                file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user.get("username"),
+                            i.get("completed"),
+                            i.get("title")
+                            )
+                        )
